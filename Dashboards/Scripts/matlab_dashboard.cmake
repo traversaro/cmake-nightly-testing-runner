dacdb17a6347c2983109f16f57c944c5ba71ade6
Silvio Traversaro <silvio.traversaro@iit.it>
2021-09-14 23:21:08 +0200

    Enable FindMatlab tests using MathWorks' GitHub Actions (#2)

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -3,6 +3,7 @@ set(CTEST_SITE "ghamatlab.ami.iit.it")
 set(CTEST_BUILD_NAME "Linux-GCC-Matlab")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_cache "CMake_TEST_FindMatlab:BOOL=TRUE")
 # For now, do not submit 
-set(dashboard_no_submit FALSE)
+set(dashboard_no_submit TRUE)
 include(${CTEST_SCRIPT_DIRECTORY}/cmake_common.cmake)
