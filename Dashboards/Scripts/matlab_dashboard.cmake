35d247213f5da14aab0c64c8a7fc8e5a7b082c0b
Silvio Traversaro <silvio.traversaro@iit.it>
2021-09-14 11:46:58 +0200

    Update matlab_dashboard.cmake

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -4,5 +4,5 @@ set(CTEST_BUILD_NAME "Linux-GCC-Matlab")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 # For now, do not submit 
-set(dashboard_no_submit FALSE)
+set(dashboard_no_submit TRUE)
 include(${CTEST_SCRIPT_DIRECTORY}/cmake_common.cmake)
