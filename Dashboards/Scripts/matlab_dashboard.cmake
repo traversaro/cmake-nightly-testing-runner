d2cfa30e7a60bbad8bf471de9afc8e4efa967333
Silvio Traversaro <silvio.traversaro@iit.it>
2021-09-15 18:50:12 +0200

    Only run Matlab-related tests

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -3,6 +3,8 @@ set(CTEST_SITE "ghamatlab.ami.iit.it")
 set(CTEST_BUILD_NAME "Linux-GCC-Matlab")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+# Only run Matlab-related tests
+set(CTEST_TEST_ARGS "INCLUDE Matlab")
 set(dashboard_cache "CMake_TEST_FindMatlab:BOOL=TRUE")
 # For now, do not submit 
 set(dashboard_no_submit TRUE)
