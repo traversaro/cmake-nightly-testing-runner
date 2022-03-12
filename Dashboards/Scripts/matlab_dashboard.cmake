506910d6fbc16aadb320588cccfbed30928db3c4
Silvio Traversaro <silvio.traversaro@iit.it>
2022-03-12 16:42:29 +0100

    Add CTEST_NO_TEST_HOME=ON to dashboard_cache (#11)

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -5,7 +5,8 @@ set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 # Only run Matlab-related tests
 set(CTEST_TEST_ARGS INCLUDE Matlab)
-set(dashboard_cache "CMake_TEST_FindMatlab:BOOL=TRUE")
+set(dashboard_cache "CMake_TEST_FindMatlab:BOOL=TRUE
+CTEST_NO_TEST_HOME:BOOL=ON")
 # For now, do not submit 
 set(dashboard_no_submit TRUE)
 include(${CTEST_SCRIPT_DIRECTORY}/cmake_common.cmake)
