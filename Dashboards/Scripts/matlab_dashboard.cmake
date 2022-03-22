e33c71f30987a720f6b394cb4d991fbdf7af30f2
Silvio Traversaro <silvio.traversaro@iit.it>
2022-03-22 22:39:00 +0100

    Update matlab_dashboard.cmake

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -1,7 +1,6 @@
 # Client maintainer: silvio.traversaro@iit.it
 set(CTEST_SITE "ghamatlab.ami.iit.it")
 set(CTEST_BUILD_NAME "Linux-GCC-Matlab-$ENV{GHA_MATLAB_VERSION}")
-message(STATUS "===========> CTEST_BUILD_NAME: ${CTEST_BUILD_NAME}")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 # Only run Matlab-related tests
