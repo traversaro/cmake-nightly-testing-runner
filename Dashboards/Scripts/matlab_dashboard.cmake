211ac45967a506098d95fe11bbe6c2261cd3bb59
Silvio Traversaro <silvio.traversaro@iit.it>
2022-03-22 22:42:38 +0100

    Test multiple MATLAB versions (#12)

--- Dashboards/Scripts/matlab_dashboard.cmake
+++ Dashboards/Scripts/matlab_dashboard.cmake
@@ -1,6 +1,6 @@
 # Client maintainer: silvio.traversaro@iit.it
 set(CTEST_SITE "ghamatlab.ami.iit.it")
-set(CTEST_BUILD_NAME "Linux-GCC-Matlab")
+set(CTEST_BUILD_NAME "Linux-GCC-Matlab-$ENV{GHA_MATLAB_VERSION}")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 # Only run Matlab-related tests
